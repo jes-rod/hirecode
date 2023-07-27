043e411125e6436d7d5fc25aa20e535d4a00d3f7
jes-rod <104109970+jes-rod@users.noreply.github.com>
2023-07-27 10:53:51 -0600

    improved apiFunctions.js

--- src/api/apiFunctions.js
+++ src/api/apiFunctions.js
@@ -20,7 +20,30 @@ export const searchJobs = async (query, country, state, remote) => {
     location = location + '+' + state;
     location = location.replaceAll(' ', '+');
   }
-  try {
+  if(!(location === '') && (rem === 'true' || rem === '')) {
+    try {
+    const response1 = await fetch (`https://corsrepo-jes-rod.vercel.app/api` , {
+        headers: {
+          Authorization: `Token ${apiKey}`,
+          'Target-URL': `https://findwork.dev/api/jobs/?location=${location}&remote=${rem}&search=${query}`
+        }
+      });
+    const jobs1 = await response1.json();
+    const response2 = await fetch (`https://corsrepo-jes-rod.vercel.app/api` , {
+        headers: {
+          Authorization: `Token ${apiKey}`,
+          'Target-URL': `https://findwork.dev/api/jobs/?location=&remote=${rem}&search=${query}`
+        }
+      });
+    const jobs2 = await response2.json();
+    const jobs = [...jobs1.results, ...jobs2.results];
+    console.log(jobs);
+    return (jobs);
+  }catch(error){
+    console.log(error);
+  }
+  }else{
+    try {
     const response = await fetch (`https://corsrepo-jes-rod.vercel.app/api` , {
         headers: {
           Authorization: `Token ${apiKey}`,
@@ -32,9 +55,7 @@ export const searchJobs = async (query, country, state, remote) => {
     return jobs.results;
   }catch(error){
     console.log(error);
-    alert('Internal server error');
-    return [];
   }
-
+  }
   
 }
