a8e68b9cd389a6154c790bf119b26ce1d7fadcce
jes-rod <104109970+jes-rod@users.noreply.github.com>
2023-07-27 08:48:31 -0600

    Update apiFunctions.js

--- src/api/apiFunctions.js
+++ src/api/apiFunctions.js
@@ -21,7 +21,7 @@ export const searchJobs = async (query, country, state, remote) => {
     location = location.replaceAll(' ', '+');
   }
   try {
-    const response = await fetch (`https://corsapp.netlify.app/.netlify/functions/api/api` , {
+    const response = await fetch (`https://corsrepo-jes-rod.vercel.app/api` , {
         headers: {
           Authorization: `Token ${apiKey}`,
           'Target-URL': `https://findwork.dev/api/jobs/?location=${location}&remote=${rem}&search=${query}`
